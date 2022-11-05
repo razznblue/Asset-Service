31dec6d0a2c735c848b92a6fec4dc5ec977a457a
Kaipo <kaipojames12@gmail.com>
2022-11-05 14:41:13 -0500

    env vars and ping app every 10 mins

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
-const PORT = '8081'
+const PORT = process.env.PORT;
+
+setInterval(async () => {
+  await axios.get('https://swgu-library.onrender.com/');
+  console.log('App Pinged');
+}, 600000);
 
 app.get("/", (req, res) => {
   res.send(`SWGU MEDIA LIBRARY`);
 });
 
-app.listen(PORT || 3005, () => {
+app.listen(PORT || 8000, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
