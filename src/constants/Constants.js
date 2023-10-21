b5d31a43220917ef4dff9cd3767f7e20a4694d9a
razznblue <kaipojames12@gmail.com>
2023-10-21 01:22:17 -0500

    rebuild

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -3,9 +3,7 @@ dotenv.config();
 
 const Constants = {
   port: process.env.PORT || 8000,
-  baseurl: process.env.NODE_ENV === 'production' 
-    ? process.env.BASE_URL
-    : `http://localhost:${process.env.PORT}`,
+  baseurl: process.env.BASE_URL,
   categories: ['IMAGES', 'AUDIO', 'JSON']
 }
 
